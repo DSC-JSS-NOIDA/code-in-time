cdbc7172c62ebfb5a49fc6f5ba4b1f02b3e7151b
shashaa35 <shashaa35@gmail.com>
2017-03-29 00:17:49 +0530

    fx landing page text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Code In Time</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,7 +69,8 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Questions</a>
+                        <a href="{{ url('/login') }}">Logout</a> 
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
@@ -79,15 +80,19 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Code In Time
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Questions</a>
+                        <a href="{{ url('/rules') }}">Rules</a>
+                        <a href="{{ url('/leaderboard') }}">Leaderboard</a>
+                        
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
                 </div>
             </div>
         </div>
